c5464dce8dce1eca438669f94cc24f5ab72b56bf
rrctechdep <274493359+rrctechgit@users.noreply.github.com>
2026-06-18 13:15:03 +0530

    inital

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const chatRoutes = require('./routes/chatRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..')));
 
 app.use('/api', chatRoutes);
 
